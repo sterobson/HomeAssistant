d66067e36c7bdc9459085764985bcbfdb289f449
Ste Robson <sterobson@users.noreply.github.com>
2025-10-31 08:19:41 +0000

    Halloween porch lights and sounds working.

---
+++ apps/HassModel/Halloween/Wooooooo.cs
@@ -0,0 +1,151 @@
+// Use unique namespaces for your apps if you going to share with others to avoid
+// conflicting names
+
+using HomeAssistant.apps;
+using HomeAssistantGenerated;
+using System.Threading.Tasks;
+
+namespace HassModel;
+
+/// <summary>
+///     Hello world showcase using the new HassModel API
+/// </summary>
+[NetDaemonApp]
+public class Wooooooo
+{
+    public Wooooooo(IHaContext ha, ITriggerManager triggerManager)
+    {
+        Entities entities = new(ha);
+        MyDevices myDevices = new(entities, ha);
+
+        Console.WriteLine("Testing occupancy");
+        LightEntity light = myDevices.PorchLight;
+
+        myDevices.PorchMotionSensor.StateChanges().SubscribeAsync(async e =>
+        {
+            if (e.New?.State == "on")
+            {
+                // Halloween sounds
+                DateTime now = DateTime.Now;
+                if (now.Month == 10 && now.Day == 31 && now.Hour >= 16 && now.Hour <= 21)
+                {
+                    _ = PlaySounds(myDevices);
+                    _ = FlashLight(light);
+                }
+            }
+        });
+    }
+
+    private static async Task PlaySounds(MyDevices myDevices)
+    {
+        myDevices.GamesRoomSpeaker.PlayMedia(new MediaPlayerPlayMediaParameters
+        {
+            Media = new
+            {
+                media_content_id = "http://192.168.1.188:8123/media/local/thunderstorm.mp3",
+                media_content_type = "music"
+            }
+        });
+
+        await Task.Delay(TimeSpan.FromSeconds(8));
+
+        myDevices.GamesRoomSpeaker.PlayMedia(new MediaPlayerPlayMediaParameters
+        {
+            Media = new
+            {
+                media_content_id = "http://192.168.1.188:8123/media/local/wolf.mp3",
+                media_content_type = "music"
+            }
+        });
+    }
+
+    private async Task FlashLight(LightEntity light)
+    {
+
+        System.Collections.Generic.IReadOnlyList<double>? colour = light.Attributes?.RgbColor;
+        string? effect = light.Attributes?.Effect;
+        double? currentBrightness = light.Attributes?.Brightness;
+
+        int turnOnAfter = 3;
+        int i = 0;
+        while (colour == null && effect == null && currentBrightness == null)
+        {
+            if (++i == turnOnAfter)
+            {
+                light.TurnOn();
+            }
+            else if (i > turnOnAfter)
+            {
+                return;
+            }
+
+            if (i > 0)
+            {
+                await Task.Delay(1000);
+            }
+
+            colour = light.Attributes?.RgbColor;
+            effect = light.Attributes?.Effect;
+            currentBrightness = light.Attributes?.Brightness;
+        }
+
+        await ToNewColour(light, 255, 85, 0, 50, TimeSpan.FromSeconds(1));
+
+        await ToNewColour(light, 255, 85, 0, 100, TimeSpan.FromSeconds(0.5));
+        await ToNewColour(light, 10, 255, 0, 100, TimeSpan.FromSeconds(0.5));
+        await ToNewColour(light, 255, 0, 155, 50, TimeSpan.FromSeconds(0.5));
+        await ToNewColour(light, 255, 85, 0, 80, TimeSpan.FromSeconds(5));
+
+        await ToNewColour(light, 255, 85, 0, 10, TimeSpan.FromSeconds(3));
+        await ToNewColour(light, 255, 85, 0, 100, TimeSpan.FromSeconds(3));
+
+        await ToNewColour(light, 255, 85, 0, 10, TimeSpan.FromSeconds(3));
+        await ToNewColour(light, 255, 85, 0, 100, TimeSpan.FromSeconds(3));
+
+        await ToNewColour(light, 255, 85, 0, 10, TimeSpan.FromSeconds(3));
+        await ToNewColour(light, 255, 85, 0, 100, TimeSpan.FromSeconds(3));
+
+        if (colour == null && effect != null)
+        {
+            light.TurnOn(effect: effect);
+        }
+        else if (colour != null)
+        {
+            await ToNewColour(light, colour[0], colour[1], colour[2], (currentBrightness ?? 100) * 100 / 255, TimeSpan.FromSeconds(3));
+        }
+    }
+
+    private static async Task ToNewColour(LightEntity light, double r, double g, double b, double brightnessPercent, TimeSpan timeToTake)
+    {
+        System.Collections.Generic.IReadOnlyList<double>? colour = light.Attributes?.RgbColor;
+        double currentBrightness = (100 * light.Attributes?.Brightness / 255) ?? 0;
+
+        double initialR = colour?[0] ?? 255;
+        double initialG = colour?[1] ?? 255;
+        double initialB = colour?[2] ?? 255;
+
+        // 5ms increments
+        int stepMs = 100;
+        int totalSteps = (int)Math.Floor(timeToTake.TotalMilliseconds / stepMs);
+
+        double dr = (r - initialR) / totalSteps;
+        double dg = (g - initialG) / totalSteps;
+        double db = (b - initialB) / totalSteps;
+        double dbr = (brightnessPercent - currentBrightness) / totalSteps;
+
+        int step = 1;
+        while (step <= totalSteps)
+        {
+            int newR = (int)(initialR + (step * dr));
+            int newG = (int)(initialG + (step * dg));
+            int newB = (int)(initialB + (step * db));
+            int newBright = (int)(currentBrightness + (step * dbr));
+            light.SetRgb(newR, newG, newB, newBright);
+
+            await Task.Delay(TimeSpan.FromMilliseconds(stepMs));
+            step++;
+        }
+
+    }
+
+}
---
+++ apps/MyEntities.cs
@@ -0,0 +1,104 @@
+﻿using HomeAssistantGenerated;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HomeAssistant.apps;
+
+public record ZhaEventData
+{
+    [JsonPropertyName("device_ieee")] public string? DeviceIeee { get; init; }
+    [JsonPropertyName("unique_id")] public string? UniqueId { get; init; }
+    [JsonPropertyName("endpoint_id")] public int? EndpointId { get; init; }
+    [JsonPropertyName("cluster_id")] public int? ClusterId { get; init; }
+    [JsonPropertyName("command")] public string? Command { get; init; }
+    [JsonPropertyName("args")] public JsonElement? Args { get; init; }
+}
+
+
+public class SonoffButton
+{
+    private readonly IHaContext _ha;
+    private readonly string _deviceIeee;
+
+    public SonoffButton(IHaContext ha, string deviceIeee)
+    {
+        _ha = ha;
+        _deviceIeee = deviceIeee;
+    }
+
+    public IObservable<Event<ZhaEventData>> Pressed()
+    {
+        return _ha.Events.Filter<ZhaEventData>("zha_event").Where(e => e.Data?.DeviceIeee == _deviceIeee && e.Data?.Command == "toggle");
+    }
+}
+
+public class MyDevices
+{
+    private readonly Entities _entities;
+    private readonly IHaContext _ha;
+
+    public MyDevices(Entities entities, IHaContext ha)
+    {
+        _entities = entities;
+        _ha = ha;
+    }
+
+    // Testing
+    public InputBooleanEntity AutomationTest => _entities.InputBoolean.AutomationTest;
+
+    // Living room
+
+    // Dining room
+    public SonoffButton DiningRoomDeskButton => new(_ha, "d4:48:67:ff:fe:0b:f6:0b");
+
+    // Kitchen
+
+    // Games room
+    public LightEntity GamesRoomDeskLamp => _entities.Light.WizRgbwTunable22099a;
+    public NumericSensorEntity GamesRoomDeskTemperature => _entities.Sensor.SonoffSnzb02dTemperature;
+    public NumericSensorEntity GamesRoomDeskHumidity => _entities.Sensor.SonoffSnzb02dHumidity;
+    public MediaPlayerEntity GamesRoomSpeaker => _entities.MediaPlayer.KitchenSpeaker;
+
+    // Bedroom 1
+
+
+    // Bedroom 2
+    public LightEntity BedroomTwoDeskLamp => _entities.Light.LamperionBaneOfShadows;
+
+    // Bedroom 3
+
+    // Porch
+    public LightEntity PorchLight => _entities.Light.PorchLight;
+    public BinarySensorEntity PorchMotionSensor => _entities.BinarySensor.EwelinkSnzb03pOccupancy;
+}
+
+public static class GamesRoomDeskButtonEntityExtensions
+{
+    public static IObservable<Event<ZhaEventData>> DiningRoomDeskButtonToggleStateChanged(this ButtonEntities buttonEntities, IHaContext ha)
+    {
+        return ha.Events.Filter<ZhaEventData>("zha_event").Where(e => e.Data?.DeviceIeee == "d4:48:67:ff:fe:0b:f6:0b" && e.Data?.Command == "toggle");
+    }
+}
+
+public static class LightEntityExtensions
+{
+    public static LightEntity SetRgb(this LightEntity lightEntity, int r, int g, int b)
+    {
+        lightEntity.TurnOn(rgbColor: [r, g, b]);
+        return lightEntity;
+    }
+
+    public static LightEntity SetRgb(this LightEntity lightEntity, int r, int g, int b, long brightnessPercent)
+    {
+        lightEntity.TurnOn(rgbColor: [r, g, b], brightnessPct: brightnessPercent);
+        return lightEntity;
+    }
+
+    public static LightEntity SetBrightnessPercent(this LightEntity lightEntity, long? brightnessPercent)
+    {
+        lightEntity.TurnOn(brightnessPct: brightnessPercent);
+        return lightEntity;
+    }
+
+}
